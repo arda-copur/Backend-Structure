6aa765bd192994a3334ae2591ac6760a3a0e7c1c
arda-copur <ardacopur0@gmail.com>
2025-01-18 20:46:22 +0300

    first backend

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ dotenv.config();
 
 const app = express();
 
-// Middleware to parse JSON
+
 app.use(express.json());
 
-// Routes
+
 app.use('/api/leagues', leagueRoutes);
 app.use('/api/teams', teamRoutes);
 app.use('/api/players', playerRoutes);
